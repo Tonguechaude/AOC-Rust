e96a8c23801411c9185bae621392a5ceeffcfc67
tongue <evan.challias@tonguechaude.fr>
2025-04-06 13:59:09 +0200

    day 5

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,3 +1,57 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub const GOOD_WEIGHT: f32 = 1.0;
+pub const BAD_WEIGHT: f32 = 2.0;
+
+#[derive(Debug, PartialEq)]
+pub enum Niceness {
+    Nice(u32),
+    Naughty,
+}
+
+pub struct Kid {
+    pub name: String,
+    pub niceness: Niceness,
+}
+
+impl Kid {
+    pub fn parse_row(csv_row: &str) -> Result<Kid, &'static str> {
+        // 🎁 Transform the CSV row into a Kid struct for Santa's list!
+        // 🎅 Expected CSV: "Name,GoodDeeds,BadDeeds"
+        //    Example: "Alice,3,1" -> name: "Alice", good_deeds: 3, bad_deeds: 1
+
+        // 🎁 Your code here! 🎁
+
+        let mut éléments = csv_row.split(',');
+        let name = éléments.next().unwrap().to_string();
+        let good_deeds = éléments.next().unwrap().parse().unwrap();
+        let bad_deeds = éléments.next().unwrap().parse().unwrap();
+
+        Ok(Self::new(name, good_deeds, bad_deeds))
+    }
+
+    pub fn new(name: String, good_deeds: u32, bad_deeds: u32) -> Self {
+        let niceness = if Self::is_nice(good_deeds, bad_deeds) {
+            Niceness::Nice(good_deeds)
+        } else {
+            Niceness::Naughty
+        };
+
+        Self { name, niceness }
+    }
+
+    pub fn is_nice(good_deeds: u32, bad_deeds: u32) -> bool {
+        if good_deeds == 0 && bad_deeds == 0 {
+            return false;
+        }
+
+        let good_deeds = good_deeds as f32 * GOOD_WEIGHT;
+        let bad_deeds = bad_deeds as f32 * BAD_WEIGHT;
+
+        let ratio = good_deeds / (good_deeds + bad_deeds);
+
+        ratio >= 0.75
+    }
+}
