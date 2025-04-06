439806bddac9b5da8632e8c8c8eefcbe02caf2b0
tongue <evan.challias@tonguechaude.fr>
2025-04-06 13:57:38 +0200

    day 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// TODO: Update the function argument type
+pub fn print_message(message: &String) {
+    println!("Message: {}", message);
+}
+
+// Example Usage
+pub fn main() {
+    let gift_message = String::from("Merry Christmas! Enjoy your gift!");
+    print_message(&gift_message);
+
+    println!("{}", gift_message);
 }
