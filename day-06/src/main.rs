9f0e5dc383eef42dfe1f41a5d226debe2df711c7
tongue <evan.challias@tonguechaude.fr>
2025-04-06 16:26:29 +0200

    day 6

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Write a function that returns the reference to the longer string
+// without any new allocations
+pub fn longer_wish<'a>(s1: &'a str, s2: &'a str) -> Option<&'a str> {
+    // Your code here
+    let s1_taille = s1.trim().chars().count();
+    let s2_taille = s2.trim().chars().count();
+
+    if s1_taille > s2_taille {
+        return Some(&s1);
+    } else if s1_taille < s2_taille {
+        Some(&s2)
+    } else {
+        None
+    }
+}
