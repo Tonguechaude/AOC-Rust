d9f29eecb792408a6586e335911c64bb6787d9cf
tongue <evan.challias@tonguechaude.fr>
2025-04-06 16:57:36 +0200

    day 8

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
     println!("Hello, world!");
+}
+
+pub struct LogQuery<'a> {
+    logs: &'a Vec<String>,
+}
+
+impl<'a> LogQuery<'a> {
+    pub fn new(logs: &'a Vec<String>) -> Self {
+        LogQuery { logs }
+    }
+
+    pub fn search(&self, keyword: &str) -> Vec<&'a String> {
+        self.logs
+            .iter()
+            .filter(|log| log.contains(keyword))
+            .collect()
+    }
+
+    pub fn export_to_file(&self, keyword: &str, path: &str) -> std::io::Result<()> {
+        // 🎁 Your code here! 🎁
+        let mut file: File = File::create(path)?;
+        for log in self.search(keyword) {
+            writeln!(file, "{}", log)?;
+        }
+        Ok(())
+    }
 }
